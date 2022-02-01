a764ea6651be4a11e90ff212a1e29cf095106d28
achechet <achechet@gmail.com>
2022-02-01 12:09:02 +0300

    All Menu

--- run.py
+++ run.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template
 from admin.admin import admin
+from chat.chat import chat
+from dashboard.dashboard import dashboard
+from documents.documents import documents
 
 app = Flask(__name__)
 app.register_blueprint(admin, url_prefix='/admin')
+app.register_blueprint(chat, url_prefix='/chat')
+app.register_blueprint(dashboard, url_prefix='/dashboard')
+app.register_blueprint(documents, url_prefix='/documents')
 
 @app.route('/')
 def index():
